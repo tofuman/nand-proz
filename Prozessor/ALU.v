52867f302071ecea0abdb83be57409d7cabafb3e
tofu <michel@tofuman.org>
2014-08-29 18:16:43 +0200

    switched experimental Alu to the right opt codes

--- Prozessor/ALU.v
+++ Prozessor/ALU.v
@@ -1,20 +1,40 @@
-module alu (a1, a2, o, status, control);
+module alu (a1, a2, o, status, control, enable);
 input wire [7:0] a1, a2;
 output reg [7:0] o;
-input wire [2:0] control;
+input wire [4:0] control;
 output reg [2:0] status;
+input wire enable;
 
-always  @ (posedge control[0] )
+always  @ (posedge enable )
 begin
-	if (control[1] && !control[2] ) begin	
-		o = a1 + a2;
-	end else if(control[1] && control[2]) begin
-		o = a1 - a2;
-	end else if(!control[1] && control[2]) begin
-		o = a1 * a2;
-	end else if(!control[1] && !control[2]) begin
-		o = a1 | a2;
-	end
+	case (control)
+		2'b00000 :begin
+				o = a1 + a2;
+				control[0] = a1[7] & a2[7];	
+			end
+		2'b00001 : o = a1 - a2;
+		2'b00010 : o = a1 | a2;
+		2'b00011 : o = a1 & a2;
+		2'b00100 : o = a1 ^ a2;
+		2'b00101 : o = ~a2;
+                2'b00110 : o = a1 << a2;
+                2'b00111 : o = a1 >> a2;
+                2'b01000 : o = a1 >>> a2;
+                2'b01001 : o = (a1 << a2) | (a1 >> (8-a2));
+                2'b01010 : o = (a1 >> a2) | (a1 >> (8-a2));
+                2'b01011 : o = a2;
+                2'b01100 : o = 2'b00000000;
+                2'b01101 : o = 2'b00000000;
+		2'b01111 : o = a2 + a1 +control[0];
+		2'b01111 : o = a2 - a1 - control[0];
+		default : o = 2'b00000000;
+	endcase
+
+
+
+
+
+
 end
 
 endmodule
